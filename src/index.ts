9ace45970a0e2618861161b55cc5f2b99b3a1610
longjiang <121874694@qq.com>
2022-08-03 18:10:46 +0800

    入口文件移除无用文件

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,6 @@ import useMergeState from "./hooks/useMergeState";
 import useMount from "./hooks/useMount";
 import useOnce from "./hooks/useOnce";
 import useUnmount from "./hooks/useUnmount";
-import DoesArrayHaveItems from "./utils/arr/DoesArrayHaveItems";
-import deepClone from "./utils/deepClone";
-import BusinessError from "./utils/error/BusinessError";
-import HttpError from "./utils/error/HttpError";
-import PropertyError from "./utils/error/PropertyError";
-import PropertyMissError from "./utils/error/PropertyMissError";
-import PropertyTypeError from "./utils/error/PropertyTypeError";
-import trim from "./utils/str/trim";
 export {
     useListenerCallback,
     useList,
@@ -19,12 +11,4 @@ export {
     useOnce,
     useMount,
     useUnmount,
-    HttpError,
-    PropertyError,
-    PropertyMissError,
-    PropertyTypeError,
-    trim,
-    DoesArrayHaveItems,
-    BusinessError,
-    deepClone,
 }
